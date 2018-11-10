24b1c31dca7951ae9281fcb9f533acab62a9829c
doudouzhao <doudouzhao@tencent.com>
2018-11-10 20:01:18 +0800

    first commit

--- js/functions.js
+++ js/functions.js
@@ -99,43 +99,42 @@ function isPc(){
 
 
 function drawtree (ctx,startx,starty,length,angle,depth,branchWidth){
-	        var rand=Math.random,
-	        n_length,n_angle,n_depth,maxbranch=6,
-	        endx,endy,maxangle=1.5*Math.PI / 4;
-	        var subbranch;
-	        ctx.beginPath();
-	        ctx.moveTo(startx,starty);
-	        endx=startx + length * Math.cos(angle);
-	        endy=starty + length * Math.sin(angle);
-	        ctx.lineCap='round';
-	        ctx.lineWidth=branchWidth;
-	        ctx.lineTo(endx,endy);
-	        if(depth<=2){
-	            //树的枝干
-	            ctx.strokeStyle= 'rgb(0,' + (((rand() * 64) +128) >>0) + ',0)';
-	        }
-	        else{
-	            //树的叶子
-	            ctx.strokeStyle= 'rgb(0,' + (((rand() * 64) +64) >>0) + ',50,25)';
-	        }
-	        ctx.stroke();
-	        n_depth = depth-1;
-	        //判断树是否结束
-	        if(!n_depth){
-	            return;
-	        }
-	        subbranch= (rand() * (maxbranch-1)) + 1;
-	        branchWidth *=0.7; 
-	        for(var i=0;i<subbranch;i++){
-	            n_angle = angle +rand() * maxangle -maxangle *0.5;
-				n_length = length * (0.5 + rand() *0.5);
-				
-	            setTimeout(function (){
-	                drawtree(ctx,endx,endy,n_length,n_angle,n_depth,branchWidth);
-	                return;
-	            }, 500)
-	        }
+	var rand=Math.random,
+	n_length,n_angle,n_depth,maxbranch=6,
+	endx,endy,maxangle=1.5*Math.PI / 4;
+	var subbranch;
+	ctx.beginPath();
+	ctx.moveTo(startx,starty);
+	endx=startx + length * Math.cos(angle);
+	endy=starty + length * Math.sin(angle);
+	ctx.lineCap='round';
+	ctx.lineWidth=branchWidth;
+	ctx.lineTo(endx,endy);
+	if(depth<=2 ){
+		//树的枝干
+		ctx.strokeStyle= 'rgb(0,' + (((rand() * 64) +128) >>0) + ',0)';
 	}
+	else{
+		//树的叶子
+		ctx.strokeStyle= 'rgb(0,' + (((rand() * 64) +64) >>0) + ',50,25)';
+	}
+	ctx.stroke();
+	n_depth = depth-1;
+	//判断树是否结束
+	if(!n_depth){
+		return;
+	}
+	subbranch= (rand() * (maxbranch-1)) + 1;
+	branchWidth *=0.7; 
+	for(var i=0;i<subbranch;i++){
+		n_angle = angle +rand() * maxangle -maxangle *0.5;
+		n_length = length * (0.5 + rand() *0.5);
+		setTimeout(function (){
+			drawtree(ctx,endx,endy,n_length,n_angle,n_depth,branchWidth);
+			return;
+		},500)
+	}
+}
 
 
 
